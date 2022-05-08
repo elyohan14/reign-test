8486b934438d957571f4c7df839be16881a8f2ff
Yohan <elyohan14@gmail.com>
2022-05-07 22:08:53 -0400

    Fix

--- src/news/news.controller.spec.ts
+++ src/news/news.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
+import { HttpModule } from '@nestjs/axios';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { New } from './entities/new.entity';
 
 describe('NewsController', () => {
   let controller: NewsController;
@@ -8,7 +11,14 @@ describe('NewsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NewsController],
-      providers: [NewsService],
+      providers: [
+        NewsService,
+        {
+          provide: getRepositoryToken(New),
+          useValue: {},
+        },
+      ],
+      imports: [HttpModule],
     }).compile();
 
     controller = module.get<NewsController>(NewsController);
--- src/news/news.service.spec.ts
+++ src/news/news.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
+import { HttpModule } from '@nestjs/axios';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { New } from './entities/new.entity';
 
 describe('NewsService', () => {
   let service: NewsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsService],
+      providers: [
+        NewsService,
+        {
+          provide: getRepositoryToken(New),
+          useValue: {},
+        },
+      ],
+      imports: [HttpModule],
     }).compile();
 
     service = module.get<NewsService>(NewsService);
